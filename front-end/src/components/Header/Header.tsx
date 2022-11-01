e6c3105e32cffdec987d734b34aff16aeec6c414
Rahdoin Ben Haj Belkacem <radhoin.benhajbelkacem@external.eni.com>
2022-11-01 20:51:39 +0100

    change header

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            bifrost-test
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
